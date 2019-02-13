026b8cf91d511c9ae2e7615fb7aa53de498838bb
Sme <esahig15789@gmail.com>
2019-02-13 13:58:09 -0500

    buildtimes

---
+++ MAKE HACK_buildtime.cmd
@@ -0,0 +1,13 @@
+cd %~dp0
+
+copy FE8_clean.gba dummy.gba
+
+cd "%~dp0Event Assembler"
+
+CoreBuildTimes A FE8 -output:"%~dp0dummy.gba" -input:"%~dp0Debug.event"
+
+cd %~dp0
+
+del "oats1.gba"
+
+pause
